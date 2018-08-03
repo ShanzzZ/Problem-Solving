f81cd0877815b71aefc1c7a42abcca43dd614f30
Shan Zhao <39207218+ShanzzZ@users.noreply.github.com>
2018-08-03 16:27:51 -0400

    Add files via upload

---
+++ Utopian Tree.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        
+        for(int i = 0; i < t; i++){
+            int n = scan.nextInt();
+            
+            if(n % 2 == 0)
+                System.out.println((int) (Math.pow(2, n/2)*2) -1);
+            else
+                System.out.println((int) ((Math.pow(2, (n-1)/2)*2) -1)*2);
+        }
+    }
+}
+		
+			
